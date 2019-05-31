5b580fbd7f8a552a75a6c67fc430fd21b38f298a
Neil Movva <neilmovva97@gmail.com>
2019-05-31 03:20:18 -0700

    typo

--- pifetch.py
+++ pifetch.py
@@ -29,7 +29,7 @@ def main(argv):
             if "range_hue" not in cam_params:
                 continue
 
-        camera.shutter = cam_params["shutter"]
+        camera.exposure_speed = cam_params["shutter"]
         camera.iso = cam_params["iso"]
 
         image = np.empty((capture_res[1] * capture_res[0] * 3), dtype=np.uint8)
